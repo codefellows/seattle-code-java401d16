c0fd7f9d920a985f1475a320248effb0a9da7a4f
Rey Oliva <reyjroliva@ufl.edu>
2023-01-13 15:10:25 -0800

    Upload LinkedList and Node skeleton code

---
+++ class-05/demo/linkedlist/LinkedList.java
@@ -0,0 +1,24 @@
+package datastructures.linkedlist;
+
+import datastructures.linkedlist.Node;
+
+public class LinkedList
+{
+  Node head = null;
+
+  public void insert(int value)
+  {
+    // TODO: implement me
+  }
+  public boolean includes(int value)
+  {
+    return false;  // TODO: implement me
+  }
+
+  @Override
+  public String toString()
+  {
+    return "null";  // TODO: implement me
+  }
+}
+
