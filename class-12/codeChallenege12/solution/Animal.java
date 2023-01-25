327128cc6099afe6ffb5fa92f964d6576c6ae5b7
Rey Oliva <reyjroliva@gmail.com>
2023-01-25 11:02:27 -0800

    added solution for code challenge 12

---
+++ class-12/codeChallenege12/solution/Animal.java
@@ -0,0 +1,16 @@
+
+
+public class Animal {
+  // make properties private
+  String species;
+  String name;
+
+  Animal(String species, String name) {
+    this.species = species;
+    this.name = name;
+  }
+
+  // add getter/setters
+
+  // add to string
+}
