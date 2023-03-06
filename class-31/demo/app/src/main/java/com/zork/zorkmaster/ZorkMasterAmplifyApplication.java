42d96e734e7f0747a34e7371320bb39b02efc5d7
Instructor Alex White <alex.white@codefellows.com>
2023-03-06 11:52:15 -0800

    Amplify analytics working

--- class-31/demo/app/src/main/java/com/zork/zorkmaster/ZorkMasterAmplifyApplication.java
+++ class-31/demo/app/src/main/java/com/zork/zorkmaster/ZorkMasterAmplifyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
+import com.amplifyframework.analytics.pinpoint.AWSPinpointAnalyticsPlugin;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
@@ -19,6 +20,7 @@ public class ZorkMasterAmplifyApplication extends Application {
         try {
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSPinpointAnalyticsPlugin());
             Amplify.addPlugin(new AWSS3StoragePlugin());
             Amplify.configure(getApplicationContext());
             Log.i(TAG, "Initialized Amplify");
--- class-31/demo/app/src/main/java/com/zork/zorkmaster/activities/MainActivity.java
+++ class-31/demo/app/src/main/java/com/zork/zorkmaster/activities/MainActivity.java
@@ -6,10 +6,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.amplifyframework.analytics.AnalyticsEvent;
 import com.amplifyframework.api.graphql.model.ModelQuery;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.SuperPet;
@@ -23,6 +25,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -36,28 +39,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int dateNow = new Date().getDate();
+
+        AnalyticsEvent appStartedEvent = AnalyticsEvent.builder()
+                .name("Application started!")
+                .addProperty("Username", "Zorkie")
+                .addProperty("TimeOfLaunch", dateNow)
+                .build();
+        Amplify.Analytics.recordEvent(appStartedEvent);
+
         setupBttns();
         setUpRecyclerView();
-
-
-//        // manual file upload to S3
-//        File exampleFile = new File(getApplicationContext().getFilesDir(), "ExampleKey");
-//
-//        try {
-//                BufferedWriter writer = new BufferedWriter(new FileWriter(exampleFile));
-//                writer.append("Example file contents");
-//                writer.close();
-//            } catch (Exception exception) {
-//                Log.e("MyAmplifyApp", "Upload failed", exception);
-//            }
-//
-//        Amplify.Storage.uploadFile(
-//                "EcampleKey",
-//                exampleFile,
-//                success -> Log.i(TAG, "FILE UPLOADED TO S3"),
-//                failure -> Log.e(TAG, "FAILED TO UPLOAD FILE" + failure)
-//        );
-
 
     }
 
